539920167a4df5a1bedd8182efa48395a19fe907
Dinh Tuan Dat <dtuandat2004@gmail.com>
2024-03-23 01:40:13 +0700

    2 type controllers

--- src/main/java/com/group4/HaUISocialMedia_server/controller/RoomTypeController.java
+++ src/main/java/com/group4/HaUISocialMedia_server/controller/RoomTypeController.java
@@ -1,50 +1,52 @@
 package com.group4.HaUISocialMedia_server.controller;
 
+import com.group4.HaUISocialMedia_server.dto.RoomTypeDto;
 import com.group4.HaUISocialMedia_server.service.RoomTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/roomType")
 public class RoomTypeController {
-//    @Autowired
-//    private RoomTypeService roomTypeService;
-//
-//    @GetMapping("/{roomTypeId}")
-//    public ResponseEntity<RoomTypeDTO> getRoomTypeById(@PathVariable UUID roomTypeId) {
-//        RoomTypeDTO res = roomTypeService.getRoomTypeById(roomTypeId);
-//        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-//        return new ResponseEntity<RoomTypeDTO>(res, HttpStatus.OK);
-//    }
-//
-//    @GetMapping("/all")
-//    public ResponseEntity<Set<RoomTypeDTO>> getAllRoomType() {
-//        Set<RoomTypeDTO> res = roomTypeService.getAllRoomTypes();
-//        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-//        return new ResponseEntity<Set<RoomTypeDTO>>(res, HttpStatus.OK);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<RoomTypeDTO> createRoomType(@RequestBody RoomTypeDTO dto) {
-//        RoomTypeDTO res = roomTypeService.createRoomType(dto);
-//        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-//        return new ResponseEntity<RoomTypeDTO>(res, HttpStatus.OK);
-//    }
-//
-//    @PutMapping
-//    public ResponseEntity<RoomTypeDTO> updateRoomType(@RequestBody RoomTypeDTO dto) {
-//        RoomTypeDTO res = roomTypeService.updateRoomType(dto);
-//        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-//        return new ResponseEntity<RoomTypeDTO>(res, HttpStatus.OK);
-//    }
-//
-//    @DeleteMapping("/{roomTypeId}")
-//    public void deleteRoomType(@PathVariable UUID roomTypeId){
-//        roomTypeService.deleteRoomType(roomTypeId);
-//    }
+    @Autowired
+    private RoomTypeService roomTypeService;
+
+    @GetMapping("/{roomTypeId}")
+    public ResponseEntity<RoomTypeDto> getRoomTypeById(@PathVariable UUID roomTypeId) {
+        RoomTypeDto res = roomTypeService.getRoomTypeById(roomTypeId);
+        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<RoomTypeDto>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Set<RoomTypeDto>> getAllRoomType() {
+        Set<RoomTypeDto> res = roomTypeService.getAllRoomTypes();
+        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<Set<RoomTypeDto>>(res, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<RoomTypeDto> createRoomType(@RequestBody RoomTypeDto dto) {
+        RoomTypeDto res = roomTypeService.createRoomType(dto);
+        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<RoomTypeDto>(res, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<RoomTypeDto> updateRoomType(@RequestBody RoomTypeDto dto) {
+        RoomTypeDto res = roomTypeService.updateRoomType(dto);
+        if (res == null) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<RoomTypeDto>(res, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{roomTypeId}")
+    public void deleteRoomType(@PathVariable UUID roomTypeId){
+        roomTypeService.deleteRoomType(roomTypeId);
+    }
 }
