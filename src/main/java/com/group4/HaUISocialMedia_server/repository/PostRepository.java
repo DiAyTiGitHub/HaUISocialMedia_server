42089a033fac98f4c468e4db847692da6434e382
Dinh Tuan Dat <dtuandat2004@gmail.com>
2024-03-24 00:51:01 +0700

    refactor

--- src/main/java/com/group4/HaUISocialMedia_server/repository/PostRepository.java
+++ src/main/java/com/group4/HaUISocialMedia_server/repository/PostRepository.java
@@ -1,13 +1,20 @@
 package com.group4.HaUISocialMedia_server.repository;
 
+import com.group4.HaUISocialMedia_server.dto.MessageDto;
+import com.group4.HaUISocialMedia_server.dto.PostDto;
 import com.group4.HaUISocialMedia_server.entity.Post;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
-
+//    @Query(value = "select new com.group4.HaUISocialMedia_server.dto.PostDto(p) from Post p where p.room.id = ?1 and m.sendDate < ?2 order by m.sendDate desc ")
+//    List<PostDto> findNext5PostFromPostId(UUID postId, Date sendDate, Pageable pageable);
 }
--- src/main/java/com/group4/HaUISocialMedia_server/service/impl/PostServiceImpl.java
+++ src/main/java/com/group4/HaUISocialMedia_server/service/impl/PostServiceImpl.java
@@ -2,16 +2,27 @@ package com.group4.HaUISocialMedia_server.service.impl;
 
 import com.group4.HaUISocialMedia_server.dto.PostDto;
 import com.group4.HaUISocialMedia_server.dto.SearchObject;
+import com.group4.HaUISocialMedia_server.entity.Post;
+import com.group4.HaUISocialMedia_server.repository.PostRepository;
 import com.group4.HaUISocialMedia_server.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class PostServiceImpl implements PostService {
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public Set<PostDto> getNewsFeed(SearchObject searchObject) {
+        Post entity = postRepository.findById(searchObject.getMileStoneId()).orElse(null);
+        Date mileStoneDate = new Date();
+        if (entity != null) mileStoneDate = entity.getCreateDate();
+//        User
         return null;
     }
 
