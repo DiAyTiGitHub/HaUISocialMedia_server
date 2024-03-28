d70078498e7f8820c8404cae31ad17d84b8da948
van hai <vanhai200345@gmail.com>
2024-03-28 16:39:16 +0700

    fix notification

--- src/main/java/com/group4/HaUISocialMedia_server/repository/NotificationRepository.java
+++ src/main/java/com/group4/HaUISocialMedia_server/repository/NotificationRepository.java
@@ -2,11 +2,16 @@ package com.group4.HaUISocialMedia_server.repository;
 
 import com.group4.HaUISocialMedia_server.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
+    @Query("SELECT n FROM Notification n where n.owner.id =:idOwner")
+    public List<Notification> findAllByUser(@Param("idOwner")UUID id);
 }
--- src/main/java/com/group4/HaUISocialMedia_server/repository/UserRepository.java
+++ src/main/java/com/group4/HaUISocialMedia_server/repository/UserRepository.java
@@ -16,4 +16,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     @Query(value = "SELECT * FROM tbl_user u WHERE u.last_name LIKE %?1% OR u.first_name LIKE %?1% OR u.username LIKE %?1% LIMIT ?2 OFFSET ?3", nativeQuery = true)
     List<User> getByUserName(String keyword, int limit, int offset);
+
+
 }
